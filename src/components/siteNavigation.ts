a65e3744afdc187dd5cec390cbd503bd8d6f9eec
dulbrich <david@ulbrichpro.com>
2026-05-18 11:04:51 -0600

    hide gallery and programs tabs for now.

--- src/components/siteNavigation.ts
+++ src/components/siteNavigation.ts
@@ -2,10 +2,8 @@ export const siteNavigation = [
   { label: "Home", href: "/" },
   { label: "About", href: "/about" },
   { label: "Veterans", href: "/veterans" },
-  { label: "Programs", href: "/programs" },
   { label: "Events", href: "/events" },
   { label: "Get Involved", href: "/get-involved" },
-  { label: "Gallery", href: "/gallery" },
   { label: "Donate", href: "/donate" },
   { label: "Contact", href: "/contact" }
 ];
